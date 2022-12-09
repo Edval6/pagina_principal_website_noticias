ccc257caf7d8a7069c8d1a0a05c8b5648d93cd11
Edival Tembe <edvaltembe@gmail.com>
2022-12-09 22:34:23 +0200

    click do menu mobile

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,25 @@ btn.addEventListener("mouseover", function(){
 btn.addEventListener("mouseout", function(){
     this.style.backgroundColor = "hsl(5, 85%, 63%)";
 });
+
+
+var close = document.querySelector('.close');
+
+close.addEventListener("click", function(){
+
+    
+    var dropbtn = document.querySelector('.dropdown-content');
+
+    dropbtn.style.display = 'none';
+
+});
+
+var show = document.querySelector('.dropbtn');
+
+show.addEventListener("click", function(){
+
+    var exibe = document.querySelector('.dropdown-content');
+
+    exibe.style.display = 'block';
+
+});
